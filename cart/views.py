979ec7fc365d92e407bf02ff5f4e3f9014405fcc
francksharif <badinisharif23@gmail.com>
2024-07-02 16:09:33 -0400

    building cart add functionality, views and templates

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+
 
 # Create your views here.
 def cart_summary(request):
-    
-    return render(request, "cart/cart_summary.html")
+    cart = Cart(request)
+    return render(request, "cart/cart_summary.html", {'cart': cart})
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        
+        cart.add(product=product, product_qty=product_quantity)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'product_qty: ': cart_quantity})
+        return response
 
 
 def cart_delete(request):
