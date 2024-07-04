fa7dbc7e6963321b7db8149bb1258ae348e298c0
francksharif <badinisharif23@gmail.com>
2024-07-04 10:28:14 -0400

    creating and setting up email verification for new users

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register', views.register, name='account-register'),
 
     # Email verification paths 
-    path('email_verification', views.register, name='email-verification'),
+    path('email_verification<str:uidb64>/<str:token>', views.email_verification, name='email-verification'),
     path('email_verification_sent', views.email_verification_sent, name='email-verification-sent'),
     path('email_verification_success', views.email_verification_success, name='email-verification-success'),
     path('email_verification_failed', views.email_verification_failed, name='email-verification-failed'),
