2d1e543695fdd33f85bc1f456326b9819b81c4ca
francksharif <badinisharif23@gmail.com>
2024-07-05 14:36:33 -0400

    creating and settingi up order and orderItem models

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from store.models import Product
 
 
 # Create your models here.
@@ -22,5 +22,34 @@ class ShippingAddress(models.Model):
         verbose_name_plural = "Shipping Address"
 
     def __str__(self):
-        return 'Shipping Adress - ' + str(self.id)
+        return 'Shipping Address - ' + str(self.id)
+    
+
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
+
+    def __str__(self):
+        return 'Order - #' + str(self.id)
+    
+
+
+
+class OrderItem(models.Model):
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+
+
+    def __str__(self):
+        return 'Order Item - #' + str(self.id)
     
