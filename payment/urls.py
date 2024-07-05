5b93ddc138c879ee3c2fef69cb6898d25973f0cf
francksharif <badinisharif23@gmail.com>
2024-07-05 14:12:43 -0400

    creating the checkout view and building the checkout functionnality and template

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('payment_success', views.success, name='payment-success'),
     path('payment_failed', views.success, name='payment-failed'),
+    path('checkout', views.checkout, name='checkout'),
 ]
